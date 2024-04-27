ae7fcba6a9cb3d04a2c59c91ee5105fe1e6c5335
kwondh5217 <trewq231@naver.com>
2024-04-27 12:30:08 +0900

    Test: AccountServiceTest 리팩토링

--- src/test/java/com/example/enjoytrip/account/service/AccountServiceTest.java
+++ src/test/java/com/example/enjoytrip/account/service/AccountServiceTest.java
@@ -6,7 +6,6 @@ import com.example.enjoytrip.account.domain.Account;
 import com.example.enjoytrip.account.domain.AccountRole;
 import com.example.enjoytrip.account.dto.AccountRequestDto;
 import com.example.enjoytrip.account.dto.AccountResponseDto;
-import com.example.enjoytrip.config.SecurityConfig;
 import com.example.enjoytrip.exception.CustomException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,55 +15,57 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Import;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
-@Import(SecurityConfig.class)
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
+    static final int VALID_ACCOUNT_ID = 1;
+    static final int SUCCESS_CODE = 1;
 
     @Mock
     AccountDao accountDao;
     AccountService accountService;
     ModelMapper modelMapper;
+    Account account;
+    AccountRequestDto requestDto;
+    AccountResponseDto responseDto;
 
     @BeforeEach
     void init () {
         modelMapper = new ModelMapper();
         accountService = new AccountServiceImpl(accountDao, modelMapper);
-    }
 
-    @DisplayName("회원을 ID로 조회하는 테스트")
-    @Test
-    void findById_success() {
-        // given
-        Integer id = 1;
-        String email = "test123@email.com";
+        String email = "test@email.com";
         String password = "pass";
         String nickname = "daeho";
         AccountRole accountRole = AccountRole.USER;
-        Account account = createAccount(id, email, password, nickname, accountRole);
+
+        account = AccountTestUtil.createAccount(VALID_ACCOUNT_ID, email, password, nickname, accountRole);
+        requestDto = AccountTestUtil.createAccountRequestDto(email, password, nickname);
+        responseDto = AccountTestUtil.createAccountResponseDto(VALID_ACCOUNT_ID, email, nickname, accountRole);
+    }
+
+    @DisplayName("일치하는 회원 ID 가 있다면, 조회 후 DTO 로 반환")
+    @Test
+    void findById_success() {
+        // given
         given(accountDao.findById(any(Integer.class))).willReturn(account);
 
         // when
-        AccountResponseDto byId = accountService.findById(id);
+        AccountResponseDto byId = accountService.findById(VALID_ACCOUNT_ID);
 
         // then
-        Assertions.assertThat(byId.getAccountId()).isEqualTo(id);
+        Assertions.assertThat(byId.getAccountId()).isEqualTo(account.getAccountId());
     }
 
-    @DisplayName("회원을 저장하는 테스트")
+    @DisplayName("회원저장 요청 시, 유니크 제약 조건 검사 후 저장하고 새로 할당된 ID 값을 반환")
     @Test
     void join_success() {
-        // given
-        String email = "test123@email.com";
-        String password = "pass";
-        String nickname = "daeho";
-        var requestDto = AccountTestUtil.createAccountRequestDto(email, password, nickname);
-
-        given(accountDao.join(any(Account.class))).willReturn(1);
+        // given : 중복되는 이메일을 가진 유저가 없을 경우만 가입이 진행됨
         given(accountDao.findByEmail(any(String.class))).willReturn(null);
+        given(accountDao.join(any(Account.class))).willReturn(VALID_ACCOUNT_ID);
 
         // when
         Integer returnCode = accountService.join(requestDto);
@@ -73,66 +74,40 @@ class AccountServiceTest {
         Assertions.assertThat(returnCode).isEqualTo(1);
     }
 
-    @DisplayName("중복된 회원 정보가 있어서 가입이 실패하는 테스트")
+    @DisplayName("회원저장 요청 시, 유니크 제약 조건이 위배된다면 custom 에러를 발생")
     @Test
     void join_fail() {
         // given
-        var requestDto = AccountTestUtil.createAccountRequestDto("test", "pass", "test");
-        given(accountDao.findByEmail(any(String.class)))
-                .willReturn(createAccount(1, "test", "test", "test", AccountRole.USER));
+        given(accountDao.findByEmail(any(String.class))).willReturn(account);
 
         // expect
-        org.junit.jupiter.api.Assertions.assertThrows(CustomException.class,
-                () -> accountService.join(requestDto));
+        assertThrows(CustomException.class, () -> accountService.join(requestDto));
     }
 
-    @DisplayName("회원을 수정하는 테스트")
+    @DisplayName("유효한 회원의 회원수정 요청 시, 성공코드를 반환")
     @Test
     void update_success() {
         // given
-        Integer id = 1;
-        String email = "test123@email.com";
-        String password = "pass";
-        String nickname = "daeho";
-        AccountRole accountRole = AccountRole.USER;
-        var account = AccountTestUtil.createAccount(id, email, password, nickname, accountRole);
-
-        given(accountDao.update(any(Account.class))).willReturn(1);
+        given(accountDao.update(any(Account.class))).willReturn(SUCCESS_CODE);
 
         // when
         Integer returnCode = accountService.update(account);
 
         // then
-        Assertions.assertThat(returnCode).isEqualTo(1);
+        Assertions.assertThat(returnCode).isEqualTo(SUCCESS_CODE);
     }
 
-    @DisplayName("회원을 삭제하는 테스트")
+    @DisplayName("유효한 회원의 회원삭제 요청 시, 성공코드를 반환")
     @Test
     void delete_success() {
         // given
-        Integer id = 1;
-        given(accountDao.delete(any(Integer.class))).willReturn(1);
+        given(accountDao.delete(any(Integer.class))).willReturn(VALID_ACCOUNT_ID);
 
         // when
-        Integer returnCode = accountService.delete(id);
+        Integer returnCode = accountService.delete(VALID_ACCOUNT_ID);
 
         // then
-        Assertions.assertThat(returnCode).isEqualTo(1);
-    }
-
-
-
-
-
-
-    private static Account createAccount(Integer id, String email, String password, String nickname, AccountRole accountRole) {
-        return Account.builder()
-                .accountId(id)
-                .accountEmail(email)
-                .accountPassword(password)
-                .accountNickname(nickname)
-                .accountRole(accountRole)
-                .build();
+        Assertions.assertThat(returnCode).isEqualTo(SUCCESS_CODE);
     }
 
 }
