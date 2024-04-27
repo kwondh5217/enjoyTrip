5c929ce9ad08546201a5c4858428f6aafce08fed
kwondh5217 <trewq231@naver.com>
2024-04-28 02:20:44 +0900

    Feat: TokenProvider 토큰 생성 기능 추가

--- src/main/java/com/example/enjoytrip/jwt/TokenProvider.java
+++ src/main/java/com/example/enjoytrip/jwt/TokenProvider.java
@@ -1,4 +1,36 @@
 package com.example.enjoytrip.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
 public class TokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private final String secretKey;
+    private final long expirationTime;
+    public SecretKey key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secretKey,
+                         @Value("${jwt.expiration-time}") long expirationTime) {
+        this.secretKey = secretKey;
+        this.expirationTime = expirationTime;
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
+    }
+
+
+    public String generateToken(Authentication authentication){
+        return Jwts.builder()
+                .subject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authentication.getAuthorities())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+expirationTime))
+                .signWith(key)
+                .compact();
+    }
 }
