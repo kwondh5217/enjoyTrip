2c7515320550391fe68b3211438a8075ec58955c
kwondh5217 <trewq231@naver.com>
2024-04-28 04:44:41 +0900

    Feat: CustomUserDetilsService loaduserByUsername 구현

--- src/main/java/com/example/enjoytrip/account/service/CustomUserDetilsService.java
+++ src/main/java/com/example/enjoytrip/account/service/CustomUserDetilsService.java
@@ -1,7 +1,11 @@
 package com.example.enjoytrip.account.service;
 
 import com.example.enjoytrip.account.dao.AccountDao;
+import com.example.enjoytrip.account.domain.Account;
+import com.example.enjoytrip.exception.CustomException;
+import com.example.enjoytrip.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,6 +19,15 @@ public class CustomUserDetilsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Account byEmail = accountDao.findByEmail(username);
+        if(byEmail == null){
+            throw new CustomException(ErrorCode.UserNotFoundException);
+        }
+
+        return User.builder()
+                .username(byEmail.getAccountEmail())
+                .password(byEmail.getAccountPassword())
+                .authorities(byEmail.getAccountRole().toString())
+                .build();
     }
 }
