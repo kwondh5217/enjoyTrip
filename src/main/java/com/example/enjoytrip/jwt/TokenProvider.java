031ebc0f9b6e6f557716455172ce842ce0f2ee53
kwondh5217 <trewq231@naver.com>
2024-04-28 04:23:16 +0900

    Feat: TokenProvider 토큰에서 권한정보 추출 기능 추가

--- src/main/java/com/example/enjoytrip/jwt/TokenProvider.java
+++ src/main/java/com/example/enjoytrip/jwt/TokenProvider.java
@@ -1,16 +1,24 @@
 package com.example.enjoytrip.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
+@Component
 public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
@@ -46,10 +54,21 @@ public class TokenProvider {
     }
 
     public String getEmailFromToken(String token){
+        return getPayload(token).getSubject();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthoritiesFromToken(String token){
+        return Arrays.stream(getPayload(token).get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+
+    private Claims getPayload(String token) {
         return Jwts.parser()
                 .verifyWith(key)
                 .build()
                 .parseSignedClaims(token)
-                .getPayload().getSubject();
+                .getPayload();
     }
 }
